0ee5bee18edf1f7a31c1f9bfa81f7dd6ed27d5af
Jhonier Jimenez <jhonier@experimentality.co>
2023-09-22 22:41:34 -0500

    Counter component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
+import React from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Learn React</p>
-        <Counter />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mount: true,
+      parentBoleean: false,
+    };
+
+    this.mountCounter = () => this.setState({ mount: true });
+    this.unmountCounter = () => this.setState({ mount: false });
+    this.setParentBoleean = () =>
+      this.setState({ parentBoleean: !this.state.parentBoleean });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Learn React</p>
+          <button onClick={this.mountCounter} disabled={this.state.mount}>
+            Mount counter
+          </button>
+          <button onClick={this.unmountCounter} disabled={!this.state.mount}>
+            Unmount counter
+          </button>
+          <button onClick={this.setParentBoleean}>Change Parent Prop</button>
+          {this.state.mount ? (
+            <Counter parentBoleean={this.state.parentBoleean} />
+          ) : null}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,36 +4,79 @@ class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      counter: 0,
     };
     console.log("Constructor is called");
+    console.log("----------------");
   }
+
+  // componentWillMount() {
+  //   console.log("componentWillMount is called");
+  //   console.log("----------------");
+  // }
+  //DEPRECATED
 
   componentDidMount() {
     console.log("ComponentDidMount is called");
+    console.log("----------------");
   }
+
+  // componentWillReceiveProps() {
+  //   console.log("componentWillReceiveProps is called");
+  //   console.log("----------------");
+  // }
+  //DEPRECATED
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextProps);
+    if (nextProps.parentBoleean) {
+      console.log("shouldComponentUpdate? YES, RENDER");
+      return true;
+    }
+    console.log("shouldComponentUpdate? NO, DO NOT RENDER");
+    return false;
+  }
+
+  // componentWillUpdate() {
+  //   console.log("componentWillUpdate is called");
+  // }
+  //DEPRECATED
 
   componentDidUpdate(prevProps, prevState) {
     console.log("ComponentDidUpdate is called");
+    console.log("----------------");
   }
 
   componentWillUnmount() {
     console.log("ComponentWillUnmount is called");
+    console.log("----------------");
   }
 
   incrementCount = () => {
+    if (!this.props.parentBoleean) return;
     this.setState((prevState) => ({
-      count: prevState.count + 1,
+      counter: prevState.counter + 1,
+    }));
+  };
+
+  decrementCount = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter - 1,
     }));
   };
 
   render() {
     console.log("Render is called");
+    console.log("----------------");
+
     return (
-      <div>
+      <div className="counter">
         <h1>Counter App</h1>
-        <p>Count: {this.state.count}</p>
+        <p>Counter: {this.state.counter}</p>
         <button onClick={this.incrementCount}>Increment</button>
+        <button onClick={this.decrementCount}>Decrement</button>
+        {console.log("PARENT BOOLEAN", this.props.parentBoleean)}
+        {this.props.parentBoleean ? <p>Authorized</p> : <p>Unauthorized</p>}
       </div>
     );
   }
