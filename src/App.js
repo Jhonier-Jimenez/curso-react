8040373fe92c0534890597b9d99ccc98fde2d9a2
Jhonier Jimenez <jhonier@experimentality.co>
2023-09-30 11:56:58 -0500

    Conditional rendering example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import "./App.css";
 
+const date = new Date();
+
 function App() {
+  console.log(date);
+
+  let currentTime = date.getHours();
+
+  let saludo;
+
+  //currentTime = 22;
+
+  if (currentTime < 12) {
+    saludo = "Good morning";
+  } else if (currentTime < 18) {
+    saludo = "Good afternoon";
+  } else {
+    saludo = "Good night";
+  }
+
+  console.log(currentTime);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Learn React</p>
-      </header>
-    </div>
+    <>
+      <div className="heading">
+        <header className="App-header">
+          <p>{saludo}</p>
+        </header>
+      </div>
+    </>
   );
 }
 
