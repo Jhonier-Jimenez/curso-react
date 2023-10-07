3d8dbdf11fce0e44352d74d9894813aef570a018
Jhonier Jimenez <jhonier@experimentality.co>
2023-10-07 11:44:23 -0500

    Destructuring example completed

--- src/App.js
+++ src/App.js
@@ -2,26 +2,45 @@ import "./App.css";
 import cars from "./data";
 
 function App() {
+  const [honda, tesla] = cars;
+
+  const {
+    coloursByPopularity: [hondaTopColour],
+  } = honda;
+
+  const {
+    speedStats: { topSpeed: hondaTopSpeed },
+  } = honda;
+
+  const {
+    coloursByPopularity: [teslaTopColour],
+  } = tesla;
+
+  const {
+    speedStats: { topSpeed: teslaTopSpeed },
+  } = tesla;
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Learn React</p>
-        {/* <table>
+        <table>
           <tr>
-            <th>Brand</th>
+            <th>Model</th>
             <th>Top Speed</th>
           </tr>
           <tr>
             <td>{tesla.model}</td>
             <td>{teslaTopSpeed}</td>
             <td>{teslaTopColour}</td>
+            {/* <td>{cars[1].colorsByPopularity[0]}</td> */}
           </tr>
           <tr>
             <td>{honda.model}</td>
             <td>{hondaTopSpeed}</td>
             <td>{hondaTopColour}</td>
           </tr>
-        </table> */}
+        </table>
       </header>
     </div>
   );
