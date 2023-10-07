7e6067eb4e1cb342948e45ce64b7862930f3ab78
Jhonier Jimenez <jhonier@experimentality.co>
2023-10-07 04:13:50 -0500

    Base challange code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+import cars from "./data";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Learn React</p>
+        {/* <table>
+          <tr>
+            <th>Brand</th>
+            <th>Top Speed</th>
+          </tr>
+          <tr>
+            <td>{tesla.model}</td>
+            <td>{teslaTopSpeed}</td>
+            <td>{teslaTopColour}</td>
+          </tr>
+          <tr>
+            <td>{honda.model}</td>
+            <td>{hondaTopSpeed}</td>
+            <td>{hondaTopColour}</td>
+          </tr>
+        </table> */}
       </header>
     </div>
   );
