079d647ddb08248b1ff4eb3c9f2e1f485d906d66
Jhonier Jimenez <jhonier@experimentality.co>
2023-10-14 04:59:43 -0500

    useState practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  setInterval(updateTime, 1000);
+
+  const now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <p>Learn React</p>
+        <div className="container">
+          <h1>{time}</h1>
+          <button onClick={updateTime}>Get Time</button>
+        </div>
       </header>
     </div>
   );
